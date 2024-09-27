39d56c89af6975f850204f40903ad99ed4c09837
Michele Catena <michele.catena@avanade.com>
2024-09-27 14:58:01 +0200

    Init commit

---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './site-header.component.html',
+  styleUrl: './site-header.component.css',
+})
+export class SiteHeaderComponent {}
